c8f2580366d7efe094cf40e3a1014a11b6dc67a9
Aleksandar <aleksandar12077171@gmail.com>
2023-04-30 17:29:34 +0200

    Update PortfolioComponent.vue

--- src/components/PortfolioComponent.vue
+++ src/components/PortfolioComponent.vue
@@ -62,8 +62,8 @@ export default {
         {name: "Portfolio", id: 6, value: 1, system: 'Website', href: 'https://gilespi.github.io/Portfolio/ ', src: require("@/assets/images/portfolio-6.png")},
         {name: "Hotel Resort", id: 7, value: 1, system: 'Website', src: ("https://media.architecturaldigest.com/photos/57e42de0fe422b3e29b7e78f/1:1/w_960,c_limit/JW_LosCabos_2015_MainExterior.jpg")},
         {name: "Photo Gallery", id: 8, value: 7, system: 'Card', src: require("@/assets/images/portfolio-11.jpg")},
-        {name: "Gadget Gallery", id: 7, value: 7, system: 'App', src: require("@/assets/images/portfolio-12.jpg")},
-        {name: "Card Gallery", id: 7, value: 4, system: 'App', src: ("https://cnb1901.com/images/card-gallery-footer6.png")}
+        {name: "Gadget Gallery", id: 9, value: 7, system: 'App', src: require("@/assets/images/portfolio-12.jpg")},
+        {name: "Card Gallery", id: 10, value: 4, system: 'App', src: ("https://cnb1901.com/images/card-gallery-footer6.png")}
       ],
       categories: [
         {
